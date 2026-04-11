e4e0463f49a109fc2e7afb163cae4216bd3d2220
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-11 18:08:08 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/skopeo/SkopeoCLITest.java
+++ src/test/java/io/kestra/plugin/skopeo/SkopeoCLITest.java
@@ -11,8 +11,7 @@ import com.google.common.collect.ImmutableMap;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.LogEntry;
 import io.kestra.core.models.property.Property;
-import io.kestra.core.queues.QueueFactoryInterface;
-import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.queues.DispatchQueueInterface;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.TestsUtils;
@@ -20,8 +19,6 @@ import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
 import io.kestra.plugin.skopeo.cli.SkopeoCLI;
 
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
-import reactor.core.publisher.Flux;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -32,13 +29,12 @@ public class SkopeoCLITest {
     RunContextFactory runContextFactory;
 
     @Inject
-    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
-    private QueueInterface<LogEntry> logQueue;
+    private DispatchQueueInterface<LogEntry> logQueue;
 
     @Test
     void skopeoInspectImage() throws Exception {
         List<LogEntry> logs = new CopyOnWriteArrayList<>();
-        Flux<LogEntry> receive = TestsUtils.receive(logQueue, l -> logs.add(l.getLeft()));
+        logQueue.addListener(logs::add);
 
         var skopeoTask = SkopeoCLI.builder()
             .id(SkopeoCLI.class.getSimpleName())
@@ -58,14 +54,13 @@ public class SkopeoCLITest {
         assertThat(run.getExitCode(), is(0));
 
         TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().toLowerCase().contains("alpine"));
-        receive.blockLast();
         assertThat(List.copyOf(logs).stream().anyMatch(log -> log.getMessage() != null && log.getMessage().toLowerCase().contains("alpine")), is(true));
     }
 
     @Test
     void skopeoCopyImage() throws Exception {
         List<LogEntry> logs = new CopyOnWriteArrayList<>();
-        Flux<LogEntry> receive = TestsUtils.receive(logQueue, l -> logs.add(l.getLeft()));
+        logQueue.addListener(logs::add);
 
         var skopeoTask = SkopeoCLI.builder()
             .id(SkopeoCLI.class.getSimpleName())
@@ -86,7 +81,6 @@ public class SkopeoCLITest {
         assertThat(run.getExitCode(), is(0));
 
         TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().toLowerCase().contains("copying"));
-        receive.blockLast();
         assertThat(List.copyOf(logs).stream().anyMatch(log -> log.getMessage() != null && log.getMessage().toLowerCase().contains("copying")), is(true));
     }
 }
